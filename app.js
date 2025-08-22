0bbcfed65a73ab31268dda4e45f44a519cea55ee
SACHINGITHUB24 <155281782+SACHINGITHUB24@users.noreply.github.com>
2025-08-22 14:39:29 +0530

    Files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+
+app.set("view engine", 'ejs')
+app.use(express.urlencoded())
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.get('/', function(req,res){
+    res.render('startpage.ejs')
+})
+
+app.get('/login', function(req,res){
+    res.render('login.ejs')
+})
+
+
+app.listen(3000, function(){
+    console.log("Server Starting on Port 3000")
+})
+
+
+
